f50d7ef9aa07950f56c469e5f5d154cff300b1a4
Ricardo Martins <martins.ricardo@bcg.com>
2024-03-19 22:23:41 -0300

    new dagger version

--- README.md
+++ README.md
@@ -2,26 +2,47 @@
 
 ## Initalizing node dev environment with typescript
 `npm init`
+
 `npm install typescript ts-node tsx @types/node supertest @types/supertest vitest -D`
+
 `npm install zod dotenv`
+
 `npx tsc --init --module esnext --moduleResolution nodenext`
 
-add script to package.json
-`"test": "vitest"`
+### add scripts to package.json
+``` json
+"scripts": {
+    "test": "vitest",
+    "build": "tsc",
+    "start:dev": "tsx watch src/server.ts",
+    "start": "node dist/server.js",
+    "lint": "eslint --quiet --ext .ts src"
+  },
+```
 
 ## Adding a simple API endpoint
 `npm install fastify`
+
 `npm install mssql`
+
 `npm install -D @types/mssql`
 
 
+## Dagger
+### Adding Dagger (new version)
+`dagger init --sdk=typescript ci`
+`dagger install github.com/quartz-technology/daggerverse/node@v0.0.1`
 
-## Adding Dagger
+### Running dagger (new version)
+`dagger call publish --directory . -o test.tar`
+
+### Adding Dagger (xdc version)
 `npm install @dagger.io/dagger@0.9 -D`
 
-## Running dagger
+### Running dagger (xdc version)
 `dagger run node --loader ts-node/esm ./test.mts`
 
 ## Loading exported image to docker:
 `docker load -i node-dagger-local-image.tar`   
+
 `docker tag <image-sha> <img-name>:<img-tag>` 
---
+++ ci/dagger/src/index.ts
@@ -0,0 +1,74 @@
+/**
+ * A generated module for Ci functions
+ *
+ * This module has been generated via dagger init and serves as a reference to
+ * basic module structure as you get started with Dagger.
+ *
+ * Two functions have been pre-created. You can modify, delete, or add to them,
+ * as needed. They demonstrate usage of arguments and return types using simple
+ * echo and grep commands. The functions can be called from the dagger CLI or
+ * from one of the SDKs.
+ *
+ * The first line in this comment block is a short description line and the
+ * rest is a long description with more detail on the module's purpose or usage,
+ * if appropriate. All modules should have a short description.
+ */
+
+import { dag, Directory, object, func } from '@dagger.io/dagger'
+
+@object()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Ci {
+  /**
+   * Runs lint, tests and publishes the app image to DockerHub
+   */
+  @func()
+  async publish(directory: Directory) {
+    const db = dag
+      .container()
+      .from('mcr.microsoft.com/mssql/server:2019-latest')
+      .withEnvVariable('ACCEPT_EULA', 'Y')
+      .withEnvVariable('SA_PASSWORD', 'Password123')
+      .withExposedPort(1433)
+      .asService()
+
+    await dag
+      .node()
+      .withVersion('21')
+      .withNpm()
+      .withSource(directory)
+      .install([])
+      .container()
+      .withExec(['npm', 'run', 'lint'])
+      .withServiceBinding('sql-server', db)
+      .withEnvVariable('DB_SERVER', 'sql-server')
+      .withEnvVariable('DB_USER', 'sa')
+      .withEnvVariable('DB_PASSWORD', 'Password123')
+      .withExec(['npx', 'vitest', 'run'])
+      .stdout()
+
+    const dockerHubSecret = process.env.DOCKER_HUB_TOKEN
+    const dockerHubUsername = process.env.DOCKER_HUB_USERNAME
+
+    const shouldPublish = !!dockerHubSecret && !!dockerHubUsername
+
+    if (!shouldPublish) {
+      console.warn(`DOCKER_HUB_TOKEN env not set!`)
+      console.warn('Skipping publish to Docker Hub step...')
+    }
+    const dockerHubTokenSecret = dag.setSecret(
+      'docker-hub-secret',
+      dockerHubSecret || '',
+    )
+    const appContainer = dag.container().build(directory).withExposedPort(3000)
+
+    if (shouldPublish)
+      return await appContainer
+        .withSecretVariable('DOCKER_HUB_TOKEN', dockerHubTokenSecret)
+        .withRegistryAuth('docker.io', dockerHubUsername, dockerHubTokenSecret)
+        .publish(dockerHubUsername + '/daggerize-node')
+    else {
+      return appContainer.asTarball()
+    }
+  }
+}
